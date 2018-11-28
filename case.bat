5ebc5020be8ce8cace70d730c12158f19a52314f
dymaxionkim <dymaxion.kim@gmail.com>
2018-11-28 14:07:13 +0900

    Deprecate AutoFEA Functions

--- case.bat
+++
@@ -1,6 +0,0 @@
-gmsh -2 case.geo
-ElmerGrid 14 2 case.msh
-move .\case\*.* .\
-rmdir case
-ElmerSolver case.sif
-gmsh case_output.msh
--- case.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-gmsh -2 ./case.geo
-ElmerGrid 14 2 case.msh
-mv ./case/* ./
-rmdir case
-ElmerSolver ./case.sif
-gmsh ./case_output.msh
